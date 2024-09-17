ae0f871901dc16338ee0477554c250db3a07383b
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-09-17 08:10:09 -0300

    authUser - token expired

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -1,8 +1,19 @@
 <?php
+
+
 const CONF_DB_HOST = "mysql";
+// const CONF_DB_HOST = "localhost";
+
 const CONF_DB_USER = "root";
+
 const CONF_DB_PASS = "asdf1234";
-// aqui deve ser alterado para o nome do banco de dados
+// const CONF_DB_PASS = "";
+
 const CONF_DB_NAME = "db-inf-3at";
+// const CONF_DB_NAME = "db-seu-banco";
+
 const CONF_URL_TEST = "http://localhost:8080/mvc-project-tarde";
+// const CONF_URL_TEST = "http://localhost/sua-pasta";
+
 const CONF_URL_BASE = "http://localhost:8080/mvc-project-tarde";
+// const CONF_URL_BASE = "http://localhost/sua-pasta";
